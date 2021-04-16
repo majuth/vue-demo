a8b51a198db98a40f076b2035befa844896636fc
Majuth <majuth101@hotmail.com>
2021-04-15 21:08:58 -0400

    First version

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            course: 'CPS847',
+            image: './picture.jfif',
+            condition: true,
+            addNum: 1
+        }
+    }
+})
